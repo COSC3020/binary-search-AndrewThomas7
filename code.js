4aa46b5cd9062f7f5c14812ed03663c7a7d8c1e5
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-01-24 18:28:02 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,41 +1,26 @@
 function binarySearch(list, element) {
-    var first = 0;
-    var last = list.length-1;
-    var mid = Math.floor((list.length) / 2);
-    let counter =0;
-    let added=false;
-    function tracker(){
-        if(added){
-            counter=counter+mid+1;
-            added=true;
+
+    function _binarySearch(list,element,first,mid,last){
+        
+        while(list[mid]===list[mid-1]){
+             mid-=1;
+
+        }
+        if(element==list[mid]||list[first]==list[last]){
+            return mid;
+        }
+        else if(element<list[mid]){
+            return _binarySearch(list,element,first,mid-1,last-1);
+        }
+        else if(element>list[mid]){
+            return _binarySearch(list,element,first+1,mid+1,last);
         }
         else{
-            counter+=mid;
-        }
-        
-    }
-
-    while(list[mid]===list[mid-1]){
-        if(counter!=0){
-            counter-=1;
-            mid-=1;
-        }
-        mid-=1;
-    }
-    if((list[first]==list[last]&&list.length!=0)){
-        return counter;
-    }
-    else if(element==list[mid]){
-        return mid;
-    }
-    else if(element< list[mid]){
-        return binarySearch(list.slice(first,mid), element)
-    }
-    else if(element > list[mid]){
-        tracker();
-        return binarySearch(list.slice(mid,last+1), element)+counter;
-    }
-    else{
         return -1;
+        }
+
+
     }
+    return _binarySearch(list,element,0,Math.floor(list.length/2),list.length-1)
+
 }
